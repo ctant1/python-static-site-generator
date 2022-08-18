2ce409bf99c021187b3e10ebc81dcef2b07deb39
Chris Tant <ctant3@ford.com>
2022-08-18 14:01:18 -0400

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -18,3 +18,19 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.isfile():
+                self.run_parser(path)
+
+
+    def load_parser(extension):
+        for parser in self.parsers:
+            if self.valid_extension(extension):
+                return extension
+
+        
+    def run_parser(path):
+        parser = load_parser(path.suffix)
+        if parser != None:
+            parser.parse()
+        else:
+            print("Not Implemented")
