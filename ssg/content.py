df39001b3e2015bfa3f1df422dee1f3cbf546268
Chris Tant <ctant3@ford.com>
2022-08-18 15:03:03 -0400

    Completed module

---
+++ ssg/content.py
@@ -0,0 +1,18 @@
+import re
+from typing import Mapping
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    def load(self, cls, string):
+        _, fm, content = self.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+        cls(metadata, content)
+
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data.["content"] = content
