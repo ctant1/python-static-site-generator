e0c6f81edf7facb30e86bc2e412526c96394cba3
Chris Tant <ctant3@ford.com>
2022-08-18 16:43:16 -0400

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -39,3 +39,7 @@ class MarkdownParser(Parser):
         html = markdown(content.body)
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+class ReStructuredTextParser:
+    extensions = [".rst"]
+    
